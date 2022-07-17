88514e1620a718ee4e1e1201bd3f0354605bdbed
Aleksey <trunkinam@gmail.com>
2022-07-17 20:21:03 +0600

    TwoCommit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 A = new Class1 { Name = "One" };
+            var test = A;
+
+            A.Name = "Two";
+            Console.WriteLine(test.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
